9cda9fe247c79cf765fed416b8c201d0c90816a6
Janusz <31537161+jstolorz@users.noreply.github.com>
2021-09-09 18:34:14 +0200

    Initial commit

---
+++ src/main/java/com/bluesoft/spring5webappexample/Spring5webappexampleApplication.java
@@ -0,0 +1,13 @@
+package com.bluesoft.spring5webappexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring5webappexampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Spring5webappexampleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/bluesoft/spring5webappexample/Spring5webappexampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bluesoft.spring5webappexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring5webappexampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
